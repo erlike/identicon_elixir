d3c1991f44e0c9d752271fcc9963e47ee1040229
erlike <erlike@MacBook-Pro-de-erlike.local>
2017-08-03 20:09:43 -0300

    init repository

---
+++ lib/identicon.ex
@@ -0,0 +1,17 @@
+defmodule Identicon do
+  def main (input) do
+    input
+    |> hash_input
+    |> pickcolor
+  end
+
+  def pickcolor(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input (input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+  end
+end
